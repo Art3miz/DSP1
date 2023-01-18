193d9a43f23264186d95b59047ee40ad8131d79b
Art3miz <bennettp2002@gmail.com>
2023-01-18 11:36:15 -0500

    Start

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int len;
+    ifstream fstr(argv[1]);
+    fstr >> len;
+    
+    return 0;
+}
