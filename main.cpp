6c37905380e89741fbfb5061f78c9abb6bf708d6
Art3miz <bennettp2002@gmail.com>
2023-01-18 12:05:04 -0500

    start of list implementation

--- main.cpp
+++ main.cpp
@@ -8,8 +8,16 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     int len;
+    int buff;
+    list<int> ffList; 
     ifstream fstr(argv[1]);
     fstr >> len;
-    
+    for(int i = 0; i < len; i++)
+    {
+        fstr >> buff;
+        ffList.push_back(buff);
+        
+    }
+
     return 0;
 }
